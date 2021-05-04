ed20498089672a843fc7fab9bd92365c925e53b8
Luis <lg.caceres19@gmail.com>
2021-05-04 11:12:46 -0500

    search function implemented, and displaying the result

--- src/components/SearchClient/SearchClient.js
+++ src/components/SearchClient/SearchClient.js
@@ -4,6 +4,10 @@ import firebaseDb from '/home/luis/Desktop/evelin/vet-app/src/firebase.js'
 const SearchClient = () => {
     
     const [idClients,setIdClients] = useState({});
+    const [searchWord, setSearchWord] = useState('');
+    const [selectedIds, setSelectedIds] = useState([]);
+
+    let selected=[]; // has the ids of the clients that match the search
 
     useEffect(()=> {
         firebaseDb.child('clients').on('value',snapshot=> {
@@ -12,13 +16,48 @@ const SearchClient = () => {
         })
     },[]);
 
+    const handleChange= (e) => {
+        const {value} = e.target;
+        console.log(value);
+        setSearchWord(value);
+
+    }
+
+    const search = (e) => {
+        e.preventDefault();
+        selected = Object.keys(idClients).filter( id => {
+            let nameSearched = searchWord.toLowerCase();
+            let name = idClients[id].nameClient.toLowerCase(); 
+            if(name.includes(nameSearched)){
+                return id;
+            }
+            
+        })
+        setSelectedIds(selected);
+
+        console.log(selected);
+
+
+                
+    }
+
+    /*.toLowerCase();
+            
+            if()
+            return id
+        }*/
+
 
 
     return(
         <div>
-            {Object.keys(idClients).map( id => (
-                <p>{idClients[id].nameClient}</p>
-            ))}
+            <div>
+                <input type='text' value={searchWord} onChange={handleChange} />
+                <button onClick={search}>Search</button>
+            </div>
+            <div>
+                {selectedIds.map( id => (<p>{idClients[id].nameClient}</p>))}
+            </div>
         </div>
     )
 }
