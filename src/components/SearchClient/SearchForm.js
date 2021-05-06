44d996625c4f67af205a876bdf28ab932b1815dd
Luis <lg.caceres19@gmail.com>
2021-05-06 11:10:45 -0500

    fetching data to the FormClient component

--- src/components/SearchClient/SearchForm.js
+++ src/components/SearchClient/SearchForm.js
@@ -1,12 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
 
+import firebaseDb from '/home/luis/Desktop/evelin/vet-app/src/firebase.js'
+
 const SearchForm = () => {
+
+    const [idClients,setIdClients] = useState({});
+    const [clientInfo,setClientInfo] = useState({
+        name : "",
+        age: 0,
+    });
     const {id} = useParams();
+
+    //Load snapshot
+    useEffect(()=> {
+        firebaseDb.child('clients').on('value',snapshot=> {
+            if(snapshot.val()!= null)
+            setIdClients({...snapshot.val()})
+        })
+        
+    },[]);
+
+    const onClick = () => {
+        console.log(idClients[id].nameClient);
+    }
+
+    const onChange = () => {
+
+    }
+
+
+
+
     return (
         <div>
-            Hello {id}
-        </div>
+            <input type="text" value="name" onChange={onChange}/>
+            <button  onClick= {onClick}>Click </button>
+        </div> 
     );
 }
 
