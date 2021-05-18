9a216b5e2cbd03ca900da3b55a233cec026f9550
Luis <lg.caceres19@gmail.com>
2021-05-18 11:36:06 -0500

    createForm component styled using grid and flexbox

--- src/components/CreateClient/CreateForm.js
+++ src/components/CreateClient/CreateForm.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import './CreateForm.css'
 
 const CreateForm = ({data, handleChange, addClient})=> {
 
@@ -6,31 +7,39 @@ const CreateForm = ({data, handleChange, addClient})=> {
 
     return(
         <form onSubmit={addClient}>
-            <label>Nombre
+            <div className="client-data">
+                <label>Nombre </label>
                 <input type='text' value={data.nameClient} onChange={handleChange} name='nameClient'/>
-            </label>
-            <label>DNI
+                <label>DNI</label>
                 <input type='text' value={data.dni} onChange={handleChange} name='dni'/>
-            </label>
-            <label>Telefono
+                <label>Telefono</label>
                 <input type='text' value={data.phone} onChange={handleChange} name='phone'/>
-            </label>
-            <label>Nombre de Mascota
+                <label>Nombre de Mascota</label>
                 <input type='text' value={data.namePet} onChange={handleChange} name='namePet'/>
-            </label>
-            <label>Edad de Mascota
+
+            </div>
+            <div className="pet-data">
+                <label>Edad de Mascota</label>
                 <input type='text' value={data.agePet} onChange={handleChange} name='agePet'/>
-            </label>
-            <label>Sexo
-                <input type='text' value={data.sexPet} onChange={handleChange} name='sexPet'/>
-            </label>
-            <label>Especie
+                
+                <label>Sexo</label>
+                <input list="sex" value={data.sexPet} onChange={handleChange} name='sexPet' id="sexPet"/>
+                
+                <label>Especie</label>
                 <input type='text' value={data.species} onChange={handleChange} name='species'/>
-            </label>
-            <label>Raza
+                
+                <label>Raza</label>
                 <input type='text' value={data.breed} onChange={handleChange} name='breed'/>
-            </label>
-            <input type='submit' value='Guardar'/>
+                
+
+
+            </div>
+                
+           
+            
+                
+                            
+            <input className="btn" type='submit' value='Guardar'/>
 
         </form>
     )
